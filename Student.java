7f293ccef5dd4aa30dfa2c187467b5ac901bc588
Akash Bhaskar <bhaskar.akash18@gmail.com>
2019-05-19 20:15:15 +0530

    Initial commit

---
+++ Student.java
@@ -0,0 +1,15 @@
+Public class Student
+{
+	private int roll;
+	private String name;
+
+	public Student(){
+		System.out.println("We are in the Default constructor");
+		}
+
+
+		public Student(int roll, String name){
+		this.roll = roll;
+		this.name = name;
+		}
+}
